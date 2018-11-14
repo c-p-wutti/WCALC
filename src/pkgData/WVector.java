b880a59c159bdeb536e8b005601a4d362637006b
C. P. Wutti <wuttic@localhost.localdomain>
2018-11-14 20:37:31 +0100

    fixed bug, added subtraction, added invertion

--- src/pkgData/WVector.java
+++ src/pkgData/WVector.java
@@ -35,6 +35,30 @@ public class WVector {
 		}
 		
 		WVector v = new WVector(ret);
-		return return v;
+		return v;
+	}
+	
+	public WVector subtract(WVector w){
+		ArrayList<Double> ret = new ArrayList<Double>();
+	
+		int dim;
+		if(this.getComponents().size() >= w.getComponents().size() ) dim = this.getComponents().size();
+		else dim = w.getComponents().size();
+		
+		for(int i=0; i<dim; i++) {
+			if(dim > this.getComponents().size()) ret.add(w.getComponents().get(i) * (-1));
+			if(dim > w.getComponents().size()) ret.add(this.getComponents().get(i));
+			ret.add(this.getComponents().get(i) - w.getComponents().get(i));
+		}
+		
+		WVector v = new WVector(ret);
+		return v;
+	}
+	
+	public void invert() {
+		ArrayList<Double> d = new ArrayList<Double>();
+		for(int i=0; i < this.getComponents().size(); i++)
+			d.add(this.getComponents().get(i) * (-1));
+		this.setComponents(d);	
 	}
 }
